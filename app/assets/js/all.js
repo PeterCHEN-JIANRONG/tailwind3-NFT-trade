0e30efdea2ba1880e82f948656ea1e03e93e8d53
peter.chen <peter@begonia-design.com.tw>
2022-09-07 00:54:10 +0800

    add header, navber

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,4 +3,17 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
   
+  // navbar add .active
+  $("header #menuBtn").click(function (e) {
+    // e.preventDefault();
+    $("header .navbar").toggleClass("active");
+    // $(this).addClass("active");
+
+    // close navbar
+  $("header .navbar .link").click(function (e) {
+    // e.preventDefault();
+    $("header .navbar").removeClass("active");
+  });
+  
+  });
 });
